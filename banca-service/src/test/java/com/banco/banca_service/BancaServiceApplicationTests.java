e4bc1f366a014f2a1b301a9d761b03f35c283264
Ronaldo <abarcaronaldo18@gmail.com>
2026-06-16 19:08:24 -0500

    feat: estructura inicial

---
+++ banca-service/src/test/java/com/banco/banca_service/BancaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banco.banca_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ eureka-server/src/test/java/com/banco/EurekaServerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.banco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+}
